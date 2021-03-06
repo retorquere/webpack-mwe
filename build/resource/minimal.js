8fb559517af4cbcef76a5ddb5b50bc5545c6edb8
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-03-06 02:52:12 +0100

    mwe

---
+++ build/resource/minimal.js
@@ -0,0 +1,28 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+/*!********************!*\
+  !*** ./minimal.ts ***!
+  \********************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.doExport = exports.doImport = exports.detectImport = void 0;
+function detectImport() {
+    return true;
+}
+exports.detectImport = detectImport;
+async function doImport() {
+}
+exports.doImport = doImport;
+function doExport() {
+}
+exports.doExport = doExport;
+
+})();
+
+var {Translator, detectImport, doImport, doExport} = __webpack_exports__;
+/******/ })()
+;
---
+++ translators/minimal.ts
@@ -0,0 +1,11 @@
+declare const Zotero: any
+
+export function detectImport(): boolean {
+  return true
+}
+
+export async function doImport(): Promise<void> {
+}
+
+export function doExport(): void {
+}
